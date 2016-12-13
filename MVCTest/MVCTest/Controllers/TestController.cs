8ce838849648b69c1fd6462b4ad4a3880ca91a15
Alex <akremene@hotmail.com>
2016-12-13 17:54:34 -0600

    Save changes

---
+++ MVCTest/MVCTest/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SayHello()
+        {
+            Models.TestClass objInfo = new Models.TestClass();
+            objInfo.FirstName = "Alex";
+            objInfo.LastName = "Kremenetskiy";
+            objInfo.Age = "100";
+
+            return View("InfoView", objInfo);
+        }
+    }
+}
---
+++ MVCTest/MVCTest/Models/TestClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTest.Models
+{
+    public class TestClass
+    {
+        private string _FirstName;
+        private string _LastName;
+        private string _Age;
+
+        public string FirstName
+        {
+            set
+            {
+                _FirstName = value;
+            }
+            get
+            {
+                return _FirstName;
+            }
+        }
+
+        public string LastName
+        {
+            set
+            {
+                _LastName = value;
+            }
+            get
+            {
+                return _LastName;
+            }
+        }
+
+        public string Age
+        {
+            set
+            {
+                _Age = value;
+            }
+            get
+            {
+                return _Age;
+            }
+        }
+    }
+}
